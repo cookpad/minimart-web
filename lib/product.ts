055f9e4d7eaa1187efda51ffc8e5e3289d261d4d
devkeita <keita.mywork@gmail.com>
2021-08-16 13:38:36 +0900

    graphqlで商品1件を取得するための関数を作成

--- lib/product.ts
+++ lib/product.ts
@@ -20,7 +20,24 @@ const listProductsQuery = `
   }
 `;
 
+const getProductQuery = `
+  query getProduct($id: ID!) {
+    product(id: $id) {
+      id
+      name
+      description
+      price
+      imageUrl
+    }
+  }
+`;
+
 export async function listProducts(): Promise<Product[]> {
   const data = await graphqlRequest({ query: listProductsQuery });
   return data.products;
+}
+
+export async function getProduct(id: string): Promise<Product> {
+  const data = await graphqlRequest({ query: getProductQuery, variables: { id } });
+  return data.product;
 }
